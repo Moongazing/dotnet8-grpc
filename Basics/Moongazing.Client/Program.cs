36de965042c4728862510a2086a468eaa7cf2b58
Tunahan Ali Ozturk <72943708+Moongazing@users.noreply.github.com>
2023-12-17 13:23:18 +0300

    basics

---
+++ Basics/Moongazing.Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Core;
+
+Channel channel = new Channel("localhost:7777", ChannelCredentials.Insecure);
+
+
+
+try
+{
+    await channel.ConnectAsync();
+    Console.WriteLine("Channel connected with server.");
+    Console.ReadLine();
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine(ex);
+}
+finally
+{
+    if (channel is not null)
+    {
+        await channel.ShutdownAsync();
+    }
+}
---
+++ Basics/Moongazing.Server/Program.cs
@@ -0,0 +1,33 @@
+﻿using Grpc.Core;
+
+Server server = new Server()
+{
+
+    Ports =
+    {
+        new ServerPort("localhost", 7777, ServerCredentials.Insecure)
+    },
+    Services =
+    {
+
+    }
+};
+
+try
+{
+    server.Start();
+    Console.WriteLine("Server started");
+    Console.ReadLine();
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine(ex);
+}
+finally
+{
+    if (server is not null)
+    {
+        await server.ShutdownAsync();
+    }
+}
